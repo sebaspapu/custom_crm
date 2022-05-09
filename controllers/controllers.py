932dad3244fbcdb287c2f3bb0b3ca4d8abb27241
sebaspapu <78824345+sebaspapu@users.noreply.github.com>
2022-05-09 02:14:49 -0500

    Creando API que realiza y se integra con otros sistemas, en este caso realiza peticion get a la base de datos

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo.http import Response
 
 import json
 
+class VisitController(http.Controller):
 
+    @http.route('/api/visits', auth='public', method=['GET'], csrf=False)
+    def get_visits(self, **kw):
+        try:
+            visits = http.request.env['custom_crm.visit'].sudo().search_read([],['id','name','customer','done'])
+            res = json.dumps(visits, ensure_ascii=False).encode('utf-8')
+            return Response(res, content_type='application/json;charset=utf-8', status=200)
+        except Exception as e:
+            return Response(json.dumps({'error':str(e)}), content_type='application/json;charset=utf-8', status=500)#El error 500 es el tipico error en el servidor
